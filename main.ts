424a4088000ceae1f0e982b4aa8bd1ec905703b6
Nyeting <150313121+Nyeting@users.noreply.github.com>
2026-04-15 20:40:35 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+})
+basic.forever(function () {
+	
+})
